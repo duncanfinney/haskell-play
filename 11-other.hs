8f31442d2b19344cf2d5581e458849334df8a5c8
Duncan Finney <duncan@leveleleven.com>
2014-11-29 11:04:11 -0500

    Getting there --> Infinite set is finding an index

--- 11-other.hs
+++ 11-other.hs
@@ -14,9 +14,7 @@ hash :: String -> Int
 hash = foldl (\x y -> x*37 + (findIdx y)) 7
 
 --it is a nine letter word
---possibleAnswers = replicateM 9 letters --nine letter words
-
---"aaaaaaaaa" to "uuuuuuuuu"
+--"aaaaaaaaa" to "wwwwwwwww"
 
 --we can dermine the word at the given index in because
 --we can convert to base36 and then map the value in letters
@@ -24,28 +22,6 @@ padZeros :: Int -> String -> String
 padZeros n s
   | length s < n  = (replicate (n - length s) '0') ++ s
   | otherwise     = s
-
-base16ToGramar :: String -> String
-base16ToGramar s = map (\c ->
-  case c of
-    '0' -> 'a'
-    '1' -> 'c'
-    '2' -> 'd'
-    '3' -> 'e'
-    '4' -> 'g'
-    '5' -> 'i'
-    '6' -> 'l'
-    '7' -> 'm'
-    '8' -> 'n'
-    '9' -> 'o'
-    'a' -> 'p'
-    'b' -> 'q'
-    'c' -> 'r'
-    'd' -> 's'
-    'e' -> 't'
-    'f' -> 'u'
-    _ -> error $ show $ "Invalid base36: " ++ s
-    ) $ padZeros 9 s -- Should be 9 chars long
 
 convertFromDecimal :: Int -> Int -> String -> String
 convertFromDecimal num toBase accum
@@ -59,18 +35,12 @@ convertFromDecimal num toBase accum
     in
     convertFromDecimal remain toBase accum'
 
+getWordAtOffset i = padZeros 9 $ convertFromDecimal i 16 ""
+hashAt = hash . getWordAtOffset
 
-convertFromDecimal :: Int -> Int -> String -> String
-convertFromDecimal num toBase accum
-  | num < 0     = error "number cannot be negative"
-  | num == 0 = accum :: String
-  | num > 0  =
-    let chars  = "acdegilmnoprstuw"
-    over   = num `mod` toBase
-    remain = num `div` toBase
-    accum' = (chars !! over) : accum
-    in
-    convertFromDecimal remain toBase accum'
+expValues = map (^2) [1..]
 
-
-    
+main = do
+  case findIndex (\x -> hashAt(x) > 956446786872726) expValues of
+    Nothing -> error "Unable to find"
+    Just z -> putStrLn $ show $ expValues !! z
